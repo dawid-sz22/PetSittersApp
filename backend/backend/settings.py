6b65b2e6d9316c7b38bdb40367eee464f516ce4d
Wuja33 <dawid.szelag22@gmail.com>
2024-11-29 22:03:33 +0100

    settings changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -80,10 +80,10 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://petsittersproject.pl",
-    "http://petsittersproject.pl",
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://petsittersproject.pl",
+#     "http://petsittersproject.pl",
+# ]
 
 ROOT_URLCONF = 'backend.urls'
 
